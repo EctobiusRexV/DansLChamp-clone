00b9a19ed215861aec984405271bdb3b20176596
Mathis Rosa-Wilson <2135567@cegeplimoilou.ca>
2023-02-15 15:21:07 -0500

    SplashScreenController new Branch

---
+++ src/main/java/controllers/SplashScreenController.java
@@ -0,0 +1,78 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class SplashScreenController {
+
+    @FXML
+    private Label LabelBasMilieu;
+
+    @FXML
+    private ImageView imageBasDroite;
+
+    @FXML
+    private ImageView imageBasGauche;
+
+    @FXML
+    private ImageView imageBasMilieu;
+
+    @FXML
+    private ImageView imageHautDroite;
+
+    @FXML
+    private ImageView imageHautGauche;
+
+    @FXML
+    private ImageView imageHautMilieu;
+
+    @FXML
+    private Label labelBasDroite;
+
+    @FXML
+    private Label labelBasGauche;
+
+    @FXML
+    private Label labelHautDroite;
+
+    @FXML
+    private Label labelHautGauche;
+
+    @FXML
+    private Label labelHautMilieu;
+
+    @FXML
+    void CircuitPressed(MouseEvent event) {
+
+    }
+
+    @FXML
+    void fermerApp(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ouvrirBibliothèque(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ouvrirFichier(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ouvrirInfoApp(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ouvrirInfoCreateurs(ActionEvent event) {
+
+    }
+
+}
+
