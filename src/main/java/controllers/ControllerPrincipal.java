0041e42c564eb21f7cd71913ac51978c94d8cb94
Mathis Rosa-Wilson <2135567@cegeplimoilou.ca>
2023-02-15 16:07:51 -0500

    ControllerPrincipal + fxml

---
+++ src/main/java/controllers/ControllerPrincipal.java
@@ -0,0 +1,47 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.layout.VBox;
+
+public class ControllerPrincipal {
+
+    @FXML
+    private Group group3D;
+
+    @FXML
+    private VBox vBoxScrollPane;
+
+    @FXML
+    void FermerRetour(ActionEvent event) {
+
+    }
+
+    @FXML
+    void enregistrerCircuit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ouvrirBibliothèque(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ouvrirCircuit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ouvrirInfoApp(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ouvrirInfoCreateur(ActionEvent event) {
+
+    }
+
+}
+
