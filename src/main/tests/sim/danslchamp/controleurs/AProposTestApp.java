aac05e9bffe7c4be1ecfa0fe55073048c9c4ec1a
2166410 <2166410@cegeplimoilou.ca>
2023-03-29 17:29:20 -0400

    TestApps

--- src/main/tests/sim/danslchamp/controleurs/AProposTestApp.java
+++ src/main/tests/sim/danslchamp/controleurs/AProposTestApp.java
@@ -1,15 +1,13 @@
 package sim.danslchamp.controleurs;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 import sim.danslchamp.DansLChampApp;
 
-public class TestBibliothequeControleur extends Application {
+public class AProposTestApp extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        DansLChampApp.loadFenetre("Bibliotheque.fxml").show();
+        DansLChampApp.loadFenetre("APropos.fxml").show();
     }
 
     public static void main(String[] args) {
