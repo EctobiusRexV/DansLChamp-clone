28e234026cdbb9c23b8cd383da9dc5b157e0933b
Thierry <2140955@cegeplimoilou.ca>
2023-04-12 14:16:30 -0400

    supprimer sout daNS BOBINE

--- src/main/java/sim/danslchamp/circuit/Bobine.java
+++ src/main/java/sim/danslchamp/circuit/Bobine.java
@@ -88,15 +88,13 @@ public class Bobine extends Composant {
             if (i == 3) {
                 champ = new CubicCurve(startX, startY, startX - 20, -1*(courant.getValeur() + getHauteur()), endX + 20, -1*(courant.getValeur() + getHauteur()), endX, endY);
                 champ.setRotationAxis(new Point3D(1, 0, 0));
-                System.out.println(champ.getStartX());
-                System.out.println(champ.getStartY());
                 champ.setRotate(90);
                 champ.setLayoutY(champ.getStartY());
                 champ.setTranslateZ(champ.getControlY1());
 
             }
             champ.setStrokeWidth(4);
-            champ.setStroke(javafx.scene.paint.Color.YELLOW);
+            champ.setStroke(Color.ORANGERED);
             champ.setStrokeLineCap(StrokeLineCap.ROUND);
             champ.setFill(Color.TRANSPARENT);
             group.getChildren().add(champ);
