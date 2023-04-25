adc99e0a30bf998a908d109e5c071bd89c4627ce
2166410 <2166410@cegeplimoilou.ca>
2023-04-25 11:57:54 -0400

    Test: TestRotation rotation avec mouseDragged

--- src/main/tests/sim/danslchamp/svg/TestRotation.java
+++ src/main/tests/sim/danslchamp/svg/TestRotation.java
@@ -1,6 +1,7 @@
 package sim.danslchamp.svg;
 
 import javafx.application.Application;
+import javafx.geometry.Point2D;
 import javafx.geometry.Point3D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -13,10 +14,13 @@ import sim.danslchamp.circuit.Résistor;
 public class TestRotation extends Application {
 
     private static final double POS_POINT = 250d;
+
+    private double pointY;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         Circle point = new Circle(0, 0, 3);
-        Résistor résistor = new Résistor(0, 0, false);
+        Résistor résistor = new Résistor(0, 0, 0);
 
         AnchorPane.setLeftAnchor(point, POS_POINT);
         AnchorPane.setTopAnchor(point, POS_POINT+résistor.getJonctions()[0].getPositionXY().y);
@@ -27,23 +31,28 @@ public class TestRotation extends Application {
 
 //        group.setRotationAxis(new Point3D(250.0, 250d, 250d));
 
-        Slider slider = new Slider(0,2*Math.PI,0);
-        slider.valueProperty().addListener((c, o, v) -> {
-            if (v != null) {
-                group.setRotate(Math.toDegrees(v.doubleValue()));
+        AnchorPane anchorPane = new AnchorPane(point, group);
 
-                group.setTranslateX(-résistor.getLargeur() * (1-Math.cos(v.doubleValue()))/2);
-                group.setTranslateY(résistor.getLargeur() * Math.sin(v.doubleValue())/2);
-            }
+        anchorPane.setOnMousePressed(event -> {
+            pointY = event.getY();
         });
-        AnchorPane.setBottomAnchor(slider, 10d);
-        AnchorPane.setLeftAnchor(slider, 5d);
-        AnchorPane.setRightAnchor(slider, 5d);
 
-        AnchorPane anchorPane = new AnchorPane(point, group, slider);
+        anchorPane.setOnMouseDragged(event -> {
+            // Rotationner le résistor selon le déplacement de la souris
+            double deltaY = event.getY() - pointY;
+            rotationner(résistor, group, deltaY/100);
+
+        });
 
         primaryStage.setScene(new Scene(anchorPane, POS_POINT*2+résistor.getLargeur(), POS_POINT*2+résistor.getHauteur()));
         primaryStage.show();
+    }
+
+    private void rotationner(Résistor résistor, Group group, Number v) {
+        group.setRotate(Math.toDegrees(v.doubleValue()));
+
+        group.setTranslateX(-group.getLayoutBounds().getWidth() * (1-Math.cos(v.doubleValue()))/2);
+        group.setTranslateY(group.getLayoutBounds().getWidth() * Math.sin(v.doubleValue())/2);
     }
 
     public static void main(String[] args) {
